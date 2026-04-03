eb2b0ccdff9c3e7af54c69c51ca8b3d62c923e01
Marian Catalin Rencsik <mrencsik@greentube.com>
2026-04-03 08:45:59 +0200

    Initial commit

---
+++ android/src/main/java/com/capacitor/navigationbar/NavigationBar.java
@@ -0,0 +1,11 @@
+package com.capacitor.navigationbar;
+
+import com.getcapacitor.Logger;
+
+public class NavigationBar {
+
+    public String echo(String value) {
+        Logger.info("Echo", value);
+        return value;
+    }
+}
